53aff96df4d12b3dfbc870a406cc77b8fda0f1ee
Nazila G <42952143+nazilagundogdu@users.noreply.github.com>
2025-09-16 18:40:04 -0600

    removed unnecessary nest_asyncio from alzheimer_pipeline_a.py

--- alzheimer_pipeline_a.py
+++ alzheimer_pipeline_a.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import nest_asyncio
 from llama_index.core import SimpleDirectoryReader, Document, Settings
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
